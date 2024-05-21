5babd914dd0bd4dd0bf8cb788f1a67b5597762f6
Min RK <benjaminrk@gmail.com>
2024-05-21 15:01:04 +0200

    add windows build

--- recipe/build-basix-py.sh
+++ recipe/build-basix-py.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -ex
 cd python
 $PYTHON -m pip install --no-build-isolation --no-deps -vv .
---
+++ recipe/build-libbasix.bat
@@ -0,0 +1,21 @@
+setlocal EnableDelayedExpansion
+
+cd cpp
+
+:: Make a build folder and change to it.
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DINSTALL_RUNTIME_DEPENDENCIES=OFF ^
+      %CMAKE_ARGS% ^
+      ..
+if errorlevel 1 exit 1
+
+cmake --build . --verbose
+if errorlevel 1 exit 1
+
+cmake --install . --verbose
+if errorlevel 1 exit 1
